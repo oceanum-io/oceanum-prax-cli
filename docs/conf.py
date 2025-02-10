2aae6d04dede8ff48cde7a86404e6791342ce602
Andre Lobato <a.lobato@oceanum.science>
2025-02-11 09:46:57 +1300

    Add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+html_favicon = "favicon.ico"
 html_theme_options = {
     "collapse_navigation": False,
     "show_nav_level": 2,
